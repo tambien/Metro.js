2a754980c6e064b12b8a606358f0766ce49ede09
Yotam Mann <yotammann@gmail.com>
2013-03-29 21:23:57 -0400

    metro no longer has it's own scheduler, but uses odot's instead

--- src/Metro.js
+++ src/Metro.js
@@ -5,16 +5,6 @@
  */
 
 ( function() {
-	//create an audio context in the window
-	//should do this in some future proof way
-	window.audioContext = window.audioContext || new webkitAudioContext();
-	var audioContext = window.audioContext;
-
-	//some audio globals
-	var sampleRate = audioContext.sampleRate;
-	var bufferSize = 2048;
-	var bufferTime = bufferSize / sampleRate;
-	var samplesToSeconds = 1 / sampleRate;
 
 	//the METRONOME object
 	window.METRO = window.METRO || {};
@@ -37,8 +27,39 @@
 		"32n" : 0,
 		"32t" : 0,
 	}
+
+	//the durations of all the beats
+	var beatDurations = {
+		"1n" : 0,
+		"2n" : 0,
+		"2t" : 0,
+		"4n" : 0,
+		"4t" : 0,
+		"8n" : 0,
+		"8t" : 0,
+		"16n" : 0,
+		"16t" : 0,
+		"32n" : 0,
+		"32t" : 0,
+	}
+
+	//the durations of all the beats
+	var beatsPerMeasure = {
+		"1n" : 1,
+		"2n" : 2,
+		"2t" : 3,
+		"4n" : 4,
+		"4t" : 6,
+		"8n" : 8,
+		"8t" : 12,
+		"16n" : 16,
+		"16t" : 24,
+		"32n" : 32,
+		"32t" : 48,
+	}
+
 	//translates the subdivisions of the measure into the strings
-	var subdivisionToString = {
+	var subdivisionStrings = {
 		1 : '1n',
 		2 : '2n',
 		3 : '2t',
@@ -52,6 +73,9 @@
 		48 : '32t',
 	}
 
+	//the default subdivisions which the metronome tics on
+	var subdivisions = ["1n", '4n'];
+
 	/**************************************************************************
 	 TEMPO and TIME SIGNATURE
 	 *************************************************************************/
@@ -59,163 +83,95 @@
 	//some default values
 	var bpm = 120;
 	var timeSignature = [4, 4];
-	var timeSigRatio = (timeSignature[0] / timeSignature[1]);
-
-	//the tatum is the smallest time increment;
-	var tatum = 0;
-	//the number of tatums in a measure
-	var tatumsPerMeasure = (96 * timeSignature[0]) / timeSignature[1];
-	
 
 	//sets the tempo, either instantly or over a period of time
-	METRO.setTempo = function(bpm, curveTime) {
-		bpm = bpm;
-		//set the clock (with the optional curveTime)
-		var now = audioContext.currentTime;
-		var curve = curveTime || 0;
-		if(oscillator) {
-			oscillator.frequency.value = (bpm / 60) * (tatumsPerMeasure / 4);
+	function setTempo(bpm) {
+		var timeSigRatio = timeSignature[0] / timeSignature[1];
+		var measureInSeconds = (60 / bpm) * 4 * timeSigRatio;
+		//set the durations of all the subdivisions
+		for(subdivision in subdivisionStrings) {
+			var sub = subdivisionStrings[subdivision];
+			var BperM = beatsPerMeasure[sub];
+			var subTime = measureInSeconds / BperM;
+			beatDurations[sub] = subTime;
 		}
+		console.log(beatDurations);
 	};
+
 	//updates the time siganture
-	METRO.setTimeSignature = function() {
-		var args = Array.prototype.slice.call(arguments);
-		if(args.length === 2) {
-			timeSignature = [args[0], args[1]];
-		} else if(args.length === 1) {
-			var arr = args[0];
-			if(arr.length === 2) {
-				timeSignature = [arr[0], arr[1]];
+	function setTimeSignature(timeSig) {
+		timeSignature = timeSig;
+		//update the beats per measure object
+		for(subdivision in subdivisionStrings) {
+			//don't count 1n since that's always 1
+			if(subdivision !== '1') {
+				var beatCount = parseInt(subdivision * (timeSig[0] / timeSig[1]));
+				beatsPerMeasure[subdivisionStrings[subdivision]] = beatCount;
 			}
 		}
-		//updates the tatums per measure when the time sig changes
-		tatumsPerMeasure = (96 * timeSignature[0]) / timeSignature[1];
-		timeSigRatio = (timeSignature[0] / timeSignature[1]);
-		//update the oscillator
-		METRO.setTempo(bpm);
 	};
+
 	//state is either 'counting', 'stopped', or 'paused'
 	var state = 'stopped';
 
 	/**************************************************************************
-	 TICK
+	 ECHO
 	 *************************************************************************/
 
-	function tick(sampleTime) {
-		//test the whole note case first
-		if(tatum === 0) {
-			var count = beats['1n'] = beats['1n'] + 1;
-			var msg = new o.msg({
-				address : "/metro/1n",
-				data : count,
-				timetag : sampleTime,
-			})
+	//this function schedules a new message when one is recieved
+	function echo(msg) {
+		//get the subdivision from the address
+		var sub = msg.address.split("/")[2];
+		//increment the counter
+		var count = msg.data + 1;
+		//roll over when the counter has reached it's max
+		if(sub !== '1n') {
+			var max = beatsPerMeasure[sub];
+			count = count % max;
 		}
-		//update each of the subdivisions
-		var measurePosition = (tatum / tatumsPerMeasure) * timeSigRatio;
-		var subdivisions = [2, 3, 4, 6, 8, 12, 16, 24, 32, 48];
-		for(var i = 0, len = subdivisions.length; i < len; i++) {
-			var sub = subdivisions[i];
-			if(measurePosition * sub % 1 === 0) {
-				var subString = subdivisionToString[sub];
-				//increment the beat
-				var count = beats[subString] = beats[subString] + 1;
-				//shedule the o.msg for that beat
-				var msg = new o.msg({
-					address : "/metro/" + subString,
-					data : count,
-					timetag : sampleTime,
-				})
-				//roll back to 0 if it's reached the end of the measure
-				//make an exception for 1n so that we have a measure counter
-				if(count === sub * timeSigRatio && sub !== 1) {
-					beats[subString] = 0;
-				}
-			}
-		}
-		//increment the tatum
-		tatum++;
-		if(tatum === tatumsPerMeasure) {
-			tatum = 0;
-		}
+		//get the next beat time
+		var nextTime = msg.timetag + beatDurations[sub];
+		//schedule the new msg
+		var msg = new o.msg({
+			address : msg.address,
+			timetag : nextTime,
+			data : count,
+		})
 	}
 
 	/**************************************************************************
-	 OSCILLATOR
+	 CONTROLS
 	 *************************************************************************/
 
-	var oscillator;
-
-	METRO.start = function(optionalBPM) {
-		var tempoBPM = optionalBPM || bpm;
-		var now = audioContext.currentTime;
-		//create an oscillator at the right resolution
-		oscillator = audioContext.createOscillator();
-		//square wave
-		oscillator.type = 1;
-		//this.phase = 0;
-		tatum = 0;
-		//set bpm
-		METRO.setTempo(tempoBPM);
-		//connect the oscillator
-		oscillator.connect(scriptNode);
-		oscillator.noteOn(now);
+	METRO.start = function(args) {
+		args = args || {};
+		var tempo = args.bpm || bpm;
+		var timeSig = args.timeSignature || timeSignature;
+		var subdivision = args.subdivision || subdivisions;
+		setTimeSignature(timeSig);
+		setTempo(tempo);
+		//set the state
 		state = 'counting';
+		//schedule the first messages
+		var now = audioContext.currentTime;
+		for(var s = 0; s < subdivision.length; s++) {
+			var sub = subdivision[s];
+			var msg = new o.msg({
+				address : "/metro/" + sub,
+				timetag : now,
+				//starts with a count of 0
+				data : 0,
+			})
+		}
+		//add the msg listener
+		o.route("/metro/*", echo);
 	};
 
 	METRO.stop = function(when) {
-		when = when | audioContext.currentTime;
-		if(oscillator) {
-			oscillator.noteOff(when);
-			state = 'stopped';
-		}
-		//reset the beat counters
+
 	};
 
 	METRO.pause = function(when) {
-		when = when | audioContext.currentTime;
-		if(oscillator) {
-			oscillator.noteOff(when);
-			state = 'paused';
-		}
-	};
-	/**************************************************************************
-	 OSCILLATOR WATCHER
-	 *************************************************************************/
 
-	//the phase of the oscillator
-	var phase = 0;
-	//watch the oscillator
-	var scriptNode = audioContext.createJavaScriptNode(bufferSize, 1, 1);
-	scriptNode.connect(audioContext.destination);
-	scriptNode.onaudioprocess = function(event) {
-		if(state === 'counting') {
-			//timing
-			var playbackTime = event.playbackTime || audioContext.currentTime;
-			//process samples
-			var inputBuffer = event.inputBuffer.getChannelData(0);
-			for(var i = 0, len = inputBuffer.length; i < len; ++i) {
-				var sample = inputBuffer[i];
-				if(sample > 0) {
-					if(!phase) {
-						var sampleTime = samplesToSeconds * i + playbackTime;
-						tick(sampleTime);
-						phase = 1;
-					}
-				} else {
-					if(phase) {
-						phase = 0;
-					}
-				}
-			}
-		}
-	};
-	//utility function for converting subdivisions to milliseconds
-	subdivisionToMilliseconds = function(subdivisionString) {
-		//remove the "n" from the end
-		subdivisionString.substring(0, subdivisionString.length - 1);
-		var subdivision = parseInt(subdivisionString);
-		var beatToMS = (60 / this.get("bpm")) * (4 / subdivision) * 1000;
-		return beatToMS;
 	};
 }());
